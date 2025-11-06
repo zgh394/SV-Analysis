fce4172ffb5161ba2dee8ea7854b88cb99d11258
zgh394 <ghzhong05@163.com>
2025-11-06 11:30:25 +0800

    Update Manta_calling_sv.sh

--- Manta_calling_sv.sh
+++ Manta_calling_sv.sh
@@ -4,13 +4,13 @@ ref="Gallus_gallus.GRCg6a.dna.toplevel.fa"
 bam_dir="./bam"
 result_dir="./result"
 
-#sv calling
+## 1. configconfig
 cat $bam_dir/bam.list | while read id
    do
-## step 1. configconfig
+
 configManta.py --bam ./bam/${id}.picardrmdum.bam --referenceFasta $ref --runDir $result_dir/raw/$id 
 
- ## step 2. Call SVs
+ ## 2. Call SVs
 $result_dir/raw/$id/runWorkflow.py
 
 done
